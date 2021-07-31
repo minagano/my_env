0f91276c8eca2f36102cfcf534b8ca233916d2c8
minagano <minagano@gmail.com>
2021-07-31 13:10:12 +0100

    delete file

--- python3/dict.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("ejdict.sqlite3")
-c = conn.cursor() #コネクションからカーソルを取得してｃに入れる
-
-sql = 'SELECT * FROM items LIMIT 10'
-rows = c.execute(sql)
-for n in rows:
-    print(n)
--- python3/dict2.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-conn = sqlite3.connect("ejdict.sqlite3")
-c = conn.cursor()
-
-sql = 'SELECT * FROM sqlite_master'
-rows = c.execute(sql)
-for n in rows:
-    print(n[4])
